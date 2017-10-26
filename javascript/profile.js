a8da5437606ec40b6f2231ba4c3d6ea278bc7738
Rocktim <rocktim53@gmail.com>
2017-10-26 21:35:05 +0530

    profile slide show completed and edit

---
+++ javascript/profile.js
@@ -0,0 +1,67 @@
+var slideIndex=1;
+var click=0;
+var time1;
+
+function plusSlides(n)
+{
+	click=1;
+	showSlides(slideIndex+=n);
+}
+
+function currentslide(n)
+{
+	click=1;
+	showSlides(slideIndex=n);
+}
+
+function showSlides(n)
+{
+	var slide=document.getElementsByClassName("singleslide");
+	var bubble=document.getElementsByClassName("dot");
+
+	if(n>slide.length)
+	{
+		slideIndex=1;
+	}
+	if(n<1)
+	{
+		slideIndex=slide.length;
+	}
+
+	for(var i=0;i<slide.length;i++)
+	{
+		slide[i].style.display="none";
+		bubble[i].className=bubble[i].className.replace(" active","");
+	}
+
+	slide[slideIndex-1].style.display="block";
+	bubble[slideIndex-1].className+=" active";
+	edit=0;
+	if(click==0)
+  		time1=setTimeout(function(){showSlides(slideIndex+=1);},4000);
+  	else if(click==1)
+    	 click=0;
+} 
+
+
+
+// load view image to be posted
+ var loadFile = function(event) {
+	    var output = document.getElementsByClassName("slidimg");
+	    output[slideIndex-1].src = URL.createObjectURL(event.target.files[0]);
+		click=1;	  
+	    showSlides(slideIndex);
+	};
+
+
+function stoptime()
+{
+	clearTimeout(time1);
+	
+}
+
+function starttime()
+{
+	if(click==0)
+	showSlides(slideIndex);
+}
