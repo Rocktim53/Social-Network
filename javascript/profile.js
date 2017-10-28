b05723d605a104cc80f1fdffb3eb9a07486e810a
Rocktim <rocktim53@gmail.com>
2017-10-28 18:51:34 +0530

    Profile page completed

--- javascript/profile.js
+++ javascript/profile.js
@@ -158,3 +158,122 @@ function enablemodal3()
 	// displaying edit menu
 	document.getElementsByClassName('modal')[4].style.display='block';
 }
+
+function update1()
+{
+	var modal=document.getElementsByClassName('modal');
+	var form=document.getElementsByTagName('form');
+
+	// disabling edit menu
+	if(form[1].children[3].value.length<1 || form[1].children[7].value.length<1 || form[1].children[11].value.length<1 || form[1].children[26].value.length<1 || form[1].children[30].value.length<1)
+	{
+
+	}
+	else
+	{
+
+		// replacing content
+	document.getElementsByClassName('tcontent')[0].children[2].innerHTML=form[1].children[3].value;
+	document.getElementsByClassName('tcontent')[0].children[4].innerHTML=form[1].children[7].value;
+	document.getElementsByClassName('tcontent')[0].children[6].innerHTML=form[1].children[11].value;
+	var dob=form[1].children[15].value+" "+form[1].children[16].value+" "+form[1].children[17].value;
+	document.getElementsByClassName('tcontent')[0].children[8].innerHTML=dob;
+
+	if(form[1].children[19].checked==true)
+	{
+	
+	 document.getElementsByClassName('tcontent')[0].children[10].innerHTML="Male";
+	}
+	else
+	{
+		document.getElementsByClassName('tcontent')[0].children[10].innerHTML="Female";
+	}
+	document.getElementsByClassName('tcontent')[0].children[12].innerHTML=form[1].children[26].value;
+	document.getElementsByClassName('tcontent')[0].children[14].innerHTML=form[1].children[30].value;
+
+		modal[0].className=modal[0].className.replace(" animate"," deanimate");
+
+		setTimeout(function(){document.getElementsByClassName('modal')[0].style.display='none';},500);
+	}
+	
+
+}
+
+function update2(n)
+{
+	var modal=document.getElementsByClassName('modal');
+	var form=document.getElementsByTagName('form');
+	
+	// disabling edit menu
+	if(form[n+1].children[3].value.length<1 || form[n+1].children[7].value.length<1 || form[n+1].children[11].value.length<1 || form[n+1].children[16].value.length<1 || form[n+1].children[17].value.length<1)
+	{
+
+	}
+	else
+	{
+
+		// replacing content
+		document.getElementsByClassName('tcontent')[n].children[2].innerHTML=form[n+1].children[3].value;
+		document.getElementsByClassName('tcontent')[n].children[4].innerHTML=form[n+1].children[7].value;
+		document.getElementsByClassName('tcontent')[n].children[6].innerHTML=form[n+1].children[11].value;
+		var res=form[n+1].children[16].value+"-"+form[n+1].children[17].value;
+		document.getElementsByClassName('tcontent')[n].children[8].innerHTML=res;
+
+		modal[n].className=modal[n].className.replace(" animate"," deanimate");
+
+		setTimeout(function(){document.getElementsByClassName('modal')[n].style.display='none';},500);
+	}
+	
+}
+
+function update3()
+{
+	var modal=document.getElementsByClassName('modal');
+	var form=document.getElementsByTagName('form');
+	
+	// disabling edit menu
+	if(form[4].children[3].value.length<1 || form[4].children[7].value.length<1 || form[4].children[11].value.length<1 || form[4].children[15].value.length<1)
+	{
+
+	}
+	else
+	{
+		// replacing content
+		var form=document.getElementsByTagName('form');
+		document.getElementsByClassName('tcontent')[3].children[2].innerHTML=form[4].children[3].value;
+		document.getElementsByClassName('tcontent')[3].children[4].innerHTML=form[4].children[7].value;
+		document.getElementsByClassName('tcontent')[3].children[6].innerHTML=form[4].children[11].value;
+		document.getElementsByClassName('tcontent')[3].children[8].innerHTML=form[4].children[15].value;
+
+		modal[3].className=modal[3].className.replace(" animate"," deanimate");
+
+		setTimeout(function(){document.getElementsByClassName('modal')[3].style.display='none';},500);
+	}
+	
+}
+
+
+function update4()
+{
+	var modal=document.getElementsByClassName('modal');
+	var form=document.getElementsByTagName('form');
+	
+	// disabling edit menu
+	if(form[5].children[3].value=="Interest In" || form[5].children[7].value.length<1 || form[5].children[11].value.length<1)
+	{
+		if(form[5].children[3].value=="Interest In")
+			alert("Please select one of the following option");
+	}
+	else
+	{
+		// replacing content
+		document.getElementsByClassName('tcontent')[4].children[2].innerHTML=form[5].children[3].value;
+		document.getElementsByClassName('tcontent')[4].children[4].innerHTML=form[5].children[7].value;
+		document.getElementsByClassName('tcontent')[4].children[6].innerHTML=form[5].children[11].value;
+
+		modal[4].className=modal[4].className.replace(" animate"," deanimate");
+
+		setTimeout(function(){document.getElementsByClassName('modal')[4].style.display='none';},500);
+	}
+	
+}
